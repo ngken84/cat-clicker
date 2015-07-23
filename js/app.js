04b2775cf957d0ef7e5b92c5fbf1df69aca98e71
Ken <ng.ken84@gmail.com>
2015-07-22 23:22:04 -0700

    Started Separations of Concerns, Added nicknames

--- js/app.js
+++ js/app.js
@@ -1,10 +1,17 @@
-var ViewModel = function() {
+
+var Cat = function () {
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable('Tabby');
 	this.imgSrc = ko.observable('img/1.jpg')
+	this.nicknames = ko.observableArray(['kitty', 'lady', 'pussy']);
+}
+
+
+var ViewModel = function() {
+	this.currentCat = ko.observable( new Cat() );
 
 	this.incrementCounter = function() {
-		this.clickCount(this.clickCount() + 1);
+		this.currentCat().clickCount(this.currentCat().clickCount() + 1);
 	};
 }
 
