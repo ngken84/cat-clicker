4f596ca1c6d7ccfc727d177222fea7def11593ac
Ken <ng.ken84@gmail.com>
2015-07-22 19:16:41 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var ViewModel = function() {
+	this.clickCount = ko.observable(0);
+	this.name = ko.observable('Tabby');
+	this.imgSrc = ko.observable('img/1.jpg')
+
+	this.incrementCounter = function() {
+		this.clickCount(this.clickCount() + 1);
+	};
+}
+
+ko.applyBindings(new ViewModel());
