3693412dba302e7735402129935b4bf9db4ee460
Ken <ng.ken84@gmail.com>
2015-07-28 23:37:29 -0700

    Add multiple cats

--- js/app.js
+++ js/app.js
@@ -1,14 +1,44 @@
 
-var Cat = function () {
-	this.clickCount = ko.observable(0);
-	this.name = ko.observable('Tabby');
-	this.imgSrc = ko.observable('img/1.jpg')
-	this.nicknames = ko.observableArray(['kitty', 'lady', 'pussy']);
+var initialCats = [
+	{
+		clickCount : 0,
+		name : 'Tabby',
+		imgSrc : 'img/1.jpg',
+		nicknames : ['kitty', 'lady', 'pussy']
+	},
+	{
+		clickCount : 0,
+		name : 'Tiger',
+		imgSrc : 'img/2.jpg',
+		nicknames : ['tiger lily']
+	},
+	{
+		clickCount : 0,
+		name : 'Scaredy',
+		imgSrc : 'img/2.jpg',
+		nicknames : ['scare crow']
+	}
+
+]
+
+
+var Cat = function (data) {
+	this.clickCount = ko.observable(data.clickCount);
+	this.name = ko.observable(data.name);
+	this.imgSrc = ko.observable(data.imgSrc)
+	this.nicknames = ko.observableArray(data.nicknames);
 }
 
 var ViewModel = function() {
 	var self = this;
-	self.currentCat = ko.observable( new Cat() );
+
+	self.catList = ko.observableArray([]);
+
+	initialCats.forEach(function(catItem) {
+		self.catList.push(new Cat(catItem));
+	})
+
+	self.currentCat = ko.observable(self.catList()[0]);
 	self.incrementCounter = function() {
 		self.currentCat().clickCount(self.currentCat().clickCount() + 1);
 	};
